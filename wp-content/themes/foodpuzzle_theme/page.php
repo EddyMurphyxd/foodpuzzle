9b64262b74a4f6308be950ce6a01bc506a2fa4e6
Vlad Fang <vlad.fang@711media.de>
2016-04-14 01:40:18 +0300

    Removed edit link button

--- wp-content/themes/foodpuzzle_theme/page.php
+++ wp-content/themes/foodpuzzle_theme/page.php
@@ -6,7 +6,6 @@
   <div class="entry-content" <?php post_class(); ?>>
     <?php the_content(); ?>
     <?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'your-theme' ) . '&after=</div>') ?>
-    <?php edit_post_link( __( 'Edit', 'your-theme' ), '<span class="edit-link">', '</span>' ) ?>
   </div><!-- .entry-content -->  
 </section> <!-- #main-section -->
  
