1d437225ab2ea3ab8922702ff0791b8f1208a9df
Vlad Fang <vlad.fang@711media.de>
2016-05-08 18:13:20 +0300

    Added seperate field for product detail image

--- wp-content/plugins/woocommerce/templates/single-product/product-image.php
+++ wp-content/plugins/woocommerce/templates/single-product/product-image.php
@@ -21,32 +21,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $post, $woocommerce, $product;
 
+$productImage = get_field('detailed_product_image');
 ?>
 <div class="images">
 	<h1 class="text-shadow"><?php echo the_title(); ?></h1>
+	<img src="<?php echo $productImage; ?>" alt="">
 	<?php
-		if ( has_post_thumbnail() ) {
-			$image_caption = get_post( get_post_thumbnail_id() )->post_excerpt;
-			$image_link    = wp_get_attachment_url( get_post_thumbnail_id() );
-			$image         = get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
-				'title'	=> get_the_title( get_post_thumbnail_id() )
-			) );
+		// if ( has_post_thumbnail() ) {
+		// 	$image_caption = get_post( get_post_thumbnail_id() )->post_excerpt;
+		// 	$image_link    = wp_get_attachment_url( get_post_thumbnail_id() );
+		// 	$image         = get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
+		// 		'title'	=> get_the_title( get_post_thumbnail_id() )
+		// 	) );
 
-			$attachment_count = count( $product->get_gallery_attachment_ids() );
+		// 	$attachment_count = count( $product->get_gallery_attachment_ids() );
 
-			if ( $attachment_count > 0 ) {
-				$gallery = '[product-gallery]';
-			} else {
-				$gallery = '';
-			}
+		// 	if ( $attachment_count > 0 ) {
+		// 		$gallery = '[product-gallery]';
+		// 	} else {
+		// 		$gallery = '';
+		// 	}
 
-			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s"/>', $image_link, $image_caption ), $post->ID );
+		// 	echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s"/>', $image_link, $image_caption ), $post->ID );
 
-		} else {
+		// } else {
 
-			echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
+		// 	echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="%s" />', wc_placeholder_img_src(), __( 'Placeholder', 'woocommerce' ) ), $post->ID );
 
-		}
+		// }
 	?>
 
 	<?php do_action( 'woocommerce_product_thumbnails' ); ?>
